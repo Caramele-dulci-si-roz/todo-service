87c1eb50d00f0a6f8711db1f234088ef58d95133
andreea <andreea.matei@esolutions.ro>
2022-03-03 08:17:29 +0200

    chore[#17]: added ObjectMapper bean

---
+++ src/main/java/ro/unibuc/hello/config/MapperConfig.java
@@ -0,0 +1,21 @@
+package ro.unibuc.hello.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper().setDefaultPropertyInclusion(JsonInclude.Include.NON_ABSENT)
+				.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+				.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+				.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	}
+
+}
