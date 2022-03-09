e3ac10abeccc4aa6b69634cdfa9110b9a9a8285b
andreea <andreea.matei@esolutions.ro>
2022-03-09 09:38:28 +0200

    feat[#6]: Get tasks by projectId or/and userId

--- src/main/java/ro/unibuc/hello/data/TaskRepository.java
+++ src/main/java/ro/unibuc/hello/data/TaskRepository.java
@@ -8,5 +8,7 @@ import java.util.List;
 public interface TaskRepository extends MongoRepository<Task, String> {
 
 	List<Task> findAllByProjectId(String projectId);
+	List<Task> findAllByAssigneeId(String userId);
+	List<Task> findAllByAssigneeIdAndProjectId(String userId, String projectId);
 
 }
