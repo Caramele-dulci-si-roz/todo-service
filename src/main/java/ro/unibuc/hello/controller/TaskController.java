ebe9b0c65005889ea0ac4d00c4a9799bec8c4a3a
stkgit28 <lstk28@gmail.com>
2022-03-10 17:30:15 +0200

    feat[#8]: Assign a task to a user

--- src/main/java/ro/unibuc/hello/controller/TaskController.java
+++ src/main/java/ro/unibuc/hello/controller/TaskController.java
@@ -6,13 +6,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.unibuc.hello.data.*;
+import ro.unibuc.hello.dto.AssignUser;
 import ro.unibuc.hello.dto.CreateTask;
 import ro.unibuc.hello.dto.TaskDto;
-import ro.unibuc.hello.dto.UpdateTask;
 import ro.unibuc.hello.exception.BadRequestException;
-import ro.unibuc.hello.util.NullAwareBeanUtils;
 
-import javax.validation.Valid;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
@@ -26,6 +25,7 @@ public class TaskController {
 
 	final TaskRepository taskRepository;
 	final ObjectMapper objectMapper;
+	final UserRepository userRepository;
 
 	@GetMapping("/{id}")
 	public TaskDto getById (@PathVariable String id){
@@ -61,6 +61,7 @@ public class TaskController {
 				.setPriority(TaskPriority.MEDIUM.toString())
 				.setStatus(TaskStatus.OPEN.toString())
 				.setType(TaskType.TASK.toString());
+
 		BeanUtils.copyProperties(createTask, task);
 		taskRepository.insert(task);
 		return ResponseEntity.ok().body("The task was successfully created.");
@@ -77,16 +78,26 @@ public class TaskController {
 		}
 	}
 
-	@PutMapping("/{id}")
-	public ResponseEntity<String> update (@PathVariable String id, @RequestBody @Valid UpdateTask updateTask) {
+	@PostMapping("/asign/{id}")
+	public ResponseEntity<String> assign(@PathVariable String id,@RequestBody AssignUser assignTaskToUser ){
 		Optional<Task> optionalTask = taskRepository.findById(id);
-		if (optionalTask.isPresent()) {
-			Task task = optionalTask.get();
-			NullAwareBeanUtils.copyProperties(updateTask, task);
-			taskRepository.save(task);
-			return ResponseEntity.ok().body("The status was successfully updated.");
+		Optional<User> optionaluser = userRepository.findById(id);
+		AssignUser userAssigned = new AssignUser();
+		if(optionalTask.isPresent()){
+			Task task = new Task();
+			BeanUtils.copyProperties(optionalTask.get(), task);
+			BeanUtils.copyProperties(assignTaskToUser, userAssigned);
+			if(optionaluser.isPresent()){
+				task.setAssigneeId(userAssigned.getAssigneeId());
+				taskRepository.save(task);
+				return ResponseEntity.badRequest().body("The task has been assigned successfully!");
+			}else{
+				return ResponseEntity.badRequest().body("The task could not be assigned because there user doesn't exist!	");
+			}
+
 		} else {
-			throw new BadRequestException("There is no task with this id!");
+			return ResponseEntity.badRequest().body("There is no task with this id!");
 		}
 	}
+
 }
---
+++ src/main/java/ro/unibuc/hello/dto/AssignUser.java
@@ -0,0 +1,10 @@
+package ro.unibuc.hello.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssignUser {
+    String assigneeId;
+}
