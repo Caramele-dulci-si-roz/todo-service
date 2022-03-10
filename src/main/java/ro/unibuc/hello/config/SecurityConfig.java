54c9d2c80f36cd0c76ac1400a81022e2ea649f55
andreea <andreea.matei@esolutions.ro>
2022-03-10 15:39:08 +0200

    feat[#7]: Update a task (with validation)

--- src/main/java/ro/unibuc/hello/config/SecurityConfig.java
+++ src/main/java/ro/unibuc/hello/config/SecurityConfig.java
@@ -44,6 +44,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 )
         );
     }
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -66,8 +80,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Set permissions on endpoints
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/auth/*").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated();
 
         // Add JWT token filter
