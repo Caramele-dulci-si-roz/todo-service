3037504dfff0169d21f37c255a803e91e5fec298
Mihail Feraru <mferaru@bitdefender.com>
2022-03-16 20:13:10 +0200

    tests: Add unit testing for authentication.

--- src/main/java/ro/unibuc/hello/data/User.java
+++ src/main/java/ro/unibuc/hello/data/User.java
@@ -1,5 +1,6 @@
 package ro.unibuc.hello.data;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import java.util.Collection;
 @NoArgsConstructor
 @Getter
 @Document
+@EqualsAndHashCode
 public class User implements UserDetails {
     @Id
     String id;
