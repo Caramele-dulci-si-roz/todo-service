494be28a82b71076e759917130890d12a89d64aa
andreea <andreea.matei@esolutions.ro>
2022-03-18 09:22:00 +0200

    Added jacoco config and more tests

---
+++ src/test/java/ro/unibuc/hello/web/GlobalExceptionHandlerTest.java
@@ -0,0 +1,30 @@
+package ro.unibuc.hello.web;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import ro.unibuc.hello.exception.BadRequestException;
+
+@ExtendWith(MockitoExtension.class)
+class GlobalExceptionHandlerTest {
+
+	@InjectMocks
+	GlobalExceptionHandler globalExceptionHandler;
+
+	@Test
+	void handleBadRequestException() {
+		BadRequestException exception = new BadRequestException("TEST");
+		Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), globalExceptionHandler.handleBadRequestException(exception).getStatusCodeValue());
+		Assertions.assertEquals("TEST", globalExceptionHandler.handleBadRequestException(exception).getBody());
+	}
+
+	@Test
+	void bindException() {
+		BindException exception = new BindException(new Object(), "test");
+		Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), globalExceptionHandler.bindException(exception).getStatusCodeValue());
+	}
+}
