0bd02391bdfa5ea38fee4977f4f5ebab82aec881
andreea <andreea.matei@esolutions.ro>
2022-03-18 09:22:00 +0200

    Added jacoco config and more tests

--- src/test/java/ro/unibuc/hello/controller/TaskControllerTest.java
+++ src/test/java/ro/unibuc/hello/controller/TaskControllerTest.java
@@ -1,17 +1,21 @@
 package ro.unibuc.hello.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import ro.unibuc.hello.data.*;
 import ro.unibuc.hello.dto.CreateTask;
+import ro.unibuc.hello.dto.TaskDto;
 import ro.unibuc.hello.dto.UpdateTask;
 import ro.unibuc.hello.exception.BadRequestException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,8 +38,23 @@ class TaskControllerTest {
 
 	@Test
 	void getById() {
-		when(taskRepository.findById(any())).thenReturn(Optional.of(new Task().setId("-1")));
-		Assertions.assertEquals("-1",taskController.getById("-1").getId());
+		Task task = new Task()
+				.setId("-1")
+				.setAssigneeId("-1")
+				.setType("BUG")
+				.setStatus("OPEN")
+				.setPriority("HIGH")
+				.setCreatedAt(LocalDateTime.now())
+				.setDescription("test")
+				.setProjectId("-1")
+				.setReporterId("-1");
+
+		when(taskRepository.findById(any())).thenReturn(Optional.of(task));
+		TaskDto result = taskController.getById("-1");
+		Task taskResult = new Task();
+		BeanUtils.copyProperties(result, taskResult);
+		Assertions.assertEquals("-1", result.getId());
+		Assertions.assertTrue(EqualsBuilder.reflectionEquals(task, taskResult));
 	}
 
 	@Test
@@ -48,7 +67,25 @@ class TaskControllerTest {
 	@Test
 	void get() {
 		when(taskRepository.findAll()).thenReturn(List.of(new Task()));
-		Assertions.assertEquals(1,taskController.get(null, null).size());
+		Assertions.assertEquals(1, taskController.get(null, null).size());
+	}
+
+	@Test
+	void getByProject() {
+		when(taskRepository.findAllByProjectId(any())).thenReturn(List.of(new Task().setProjectId("-1")));
+		Assertions.assertEquals(1, taskController.get("-1", null).size());
+	}
+
+	@Test
+	void getByUser() {
+		when(taskRepository.findAllByAssigneeId(any())).thenReturn(List.of(new Task().setAssigneeId("-1")));
+		Assertions.assertEquals(1, taskController.get(null, "-1").size());
+	}
+
+	@Test
+	void getBYProjectAndUser() {
+		when(taskRepository.findAllByAssigneeIdAndProjectId(any(),any())).thenReturn(List.of(new Task().setAssigneeId("-1").setProjectId("-1")));
+		Assertions.assertEquals(1, taskController.get("-1", "-1").size());
 	}
 
 	@Test
@@ -68,15 +105,15 @@ class TaskControllerTest {
 	@Test
 	void delete() {
 		when(taskRepository.findById(any())).thenReturn(Optional.of(new Task().setId("-1")));
-		Assertions.assertEquals("The task was successfully removed.",taskController.delete("-1").getBody());
-		Assertions.assertEquals(HttpStatus.OK.value(),taskController.delete("-1").getStatusCodeValue());
+		Assertions.assertEquals("The task was successfully removed.", taskController.delete("-1").getBody());
+		Assertions.assertEquals(HttpStatus.OK.value(), taskController.delete("-1").getStatusCodeValue());
 	}
 
 	@Test
 	void deleteNotFound() {
 		when(taskRepository.findById(any())).thenReturn(Optional.empty());
-		Assertions.assertEquals("There is no task with this id!",taskController.delete("-1").getBody());
-		Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(),taskController.delete("-1").getStatusCodeValue());
+		Assertions.assertEquals("There is no task with this id!", taskController.delete("-1").getBody());
+		Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), taskController.delete("-1").getStatusCodeValue());
 	}
 
 	@Test
