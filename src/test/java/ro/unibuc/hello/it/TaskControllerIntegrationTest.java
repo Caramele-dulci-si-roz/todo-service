5279fb71141b44524c26fb2117c4acea69773e13
Mihail Feraru <mihailferaru2000@gmail.com>
2022-04-15 09:35:10 +0300

    Update TaskControllerIntegrationTest.java

--- src/test/java/ro/unibuc/hello/it/TaskControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/hello/it/TaskControllerIntegrationTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
-//@Disabled
+@Disabled
 class TaskControllerIntegrationTest {
 
 	@Autowired
