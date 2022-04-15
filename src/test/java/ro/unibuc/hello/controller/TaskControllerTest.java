fce697983c8b1579ebf31881d17db9758ee251a7
Mihail Feraru <mihailferaru2000@gmail.com>
2022-04-15 09:39:46 +0300

    Update TaskControllerTest.java

--- src/test/java/ro/unibuc/hello/controller/TaskControllerTest.java
+++ src/test/java/ro/unibuc/hello/controller/TaskControllerTest.java
@@ -1,6 +1,7 @@
 package ro.unibuc.hello.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
